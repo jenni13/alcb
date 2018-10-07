cdbf2a1cac6c374e11f6cdfba712b5bae8dfcd30
root <root@vm-dyn-0-208.siame.univ-tlse3.fr>
2018-10-07 14:05:35 +0200

    geo + ioctl

--- alcb.c
+++ alcb.c
@@ -51,11 +51,37 @@ static int rb_open(struct block_device *dev, fmode_t mode)
         printk(KERN_ALERT "Appel rb_open\n");
         return 0;
 }
-
-static int rb_getgeo(struct block_device *dev,struct hd_geometry *geo)
+static int rb_release(struct inode *inode, struct file *filp,unsigned int cmd,unsigned long arg)
 {
-        printk(KERN_ALERT "Appel de rb_getgeo\n");
+	printk(KERN_ALERT "Appel rb_release\n");
+	return 0;
+}
+
+static int rb_getgeo(struct block_device *dev, struct hd_geometry *geo)
+{
+	printk(KERN_ALERT "Appel de rb_getgeo\n");
         return 0;
+}
+
+int rb_ioctl(struct inode *inode,struct file *filp,unsigned int cmd,unsigned long arg)
+{
+	long size;
+	struct hd_geometry *geo;
+	
+	switch(cmd){
+		case HDIO_GETGEO:
+			printk(KERN_ALERT "Appel de rb_getgeo\n");
+			size =  rb_dev.size*(hardsect_size / KERNEL_SECTOR_SIZE);
+        		geo -> cylinders = (size & ~0x3f) >> 6;
+       			geo -> heads = 4;
+        		geo -> sectors = 16;
+        		geo -> start = 0;
+        		if(copy_to_user((void * ) arg,&geo,sizeof(geo)))
+                		return -EFAULT;
+			return 0;
+		default:
+			return -ENOTTY;
+	}
 }
 
 
@@ -63,8 +89,10 @@ static struct block_device_operations rb_fops =
 {
 	.owner = THIS_MODULE,
 	.open = rb_open,
-	.getgeo = rb_getgeo,
-	//.release = rb_release,
+	.release = rb_release,
+        .getgeo = rb_getgeo,
+        .ioctl = rb_ioctl,
+
 };
 
 static void rb_transfert(struct rb_device *dev, unsigned long sector,
@@ -72,7 +100,7 @@ static void rb_transfert(struct rb_device *dev, unsigned long sector,
 {
 	unsigned long offset = sector*hardsect_size;
 	unsigned long nbytes = nsect*hardsect_size;
-	
+
 	if((offset + nbytes) > dev -> size)
 	{
 		printk(KERN_NOTICE "probleme transfert\n");
@@ -103,10 +131,10 @@ static void rb_request(struct request_queue *q)
 	}
 }
 
-int blk_init(void) 
+int blk_init(void)
 {
 	printk(KERN_ALERT "Hello %s !\n", NomUtilisateur);
-	
+
 	rb_dev.size = nsectors*hardsect_size;
 	spin_lock_init(&rb_dev.lock);
 	rb_dev.data = vmalloc(rb_dev.size);
@@ -116,8 +144,8 @@ int blk_init(void)
 	if(Queue == NULL)
 		goto out;
 	//blk_queue_hardsect_size(Queue,hardsect_size);
-	
-	
+
+
 	major_num = register_blkdev(major_num,"blc");
 
 	if(major_num<=0)
@@ -125,12 +153,12 @@ int blk_init(void)
 		printk(KERN_WARNING "blc, unable to get major number\n");
 		goto out;
 	}
-	
+
 	rb_dev.rb_disk = alloc_disk(16);
 	if(!rb_dev.rb_disk)
 		goto out_unreg;
-	
-	// Structures allocation 
+
+	// Structures allocation
 	rb_dev.rb_disk->major = major_num;
 	rb_dev.rb_disk->first_minor = 0;
 	rb_dev.rb_disk->fops = &rb_fops;
@@ -147,16 +175,16 @@ int blk_init(void)
 			vfree(rb_dev.data);
 			return -ENOMEM;
 	out_unreg:
-			unregister_blkdev(major_num,"blc");
+			unregister_blkdev(major_num,"sbd0");
 			return -ENOMEM;
-	
+
 }
 
 
-static void blk_cleanup(void) 
+static void blk_cleanup(void)
 {
     printk(KERN_ALERT "Goodbye %s\n", NomUtilisateur);
-    
+
 	del_gendisk(rb_dev.rb_disk);
 	put_disk(rb_dev.rb_disk);
 	unregister_blkdev(major_num,"sdb0");
